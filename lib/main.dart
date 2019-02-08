a8322423ca453b0def964266ad0bff0ac3266463
Ovidio R. Calvet <calveto@gmail.com>
2019-02-08 01:51:39 -0500

    Add path clipping

--- lib/main.dart
+++ lib/main.dart
@@ -32,9 +32,10 @@ class Teeth extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     Path path = Path();
-    path.lineTo(10, 10);
-    path.lineTo(100, 100);
-    path.lineTo(200, 10);
+    path.quadraticBezierTo(0, 10, size.width / 2, 40);
+    // path.lineTo(10, 10);
+    // path.lineTo(100, 100);
+    // path.lineTo(200, 10);
     path.close();
     return path;
   }
